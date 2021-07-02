9b248a0a8b19d9c6d54cc4cd821b1e2360d099eb
GabrielSCardoso <gabriel.csantos@bandtec.com.br>
2021-07-02 14:23:54 -0300

    Nome de usuario adicionado

--- Python/ex058-Jogo-Adivinhacao-V2.py
+++ Python/ex058-Jogo-Adivinhacao-V2.py
@@ -4,6 +4,7 @@ computador = randint(0, 10)
 print('\033[1;31mSou seu computador... Acabei de pensar em um número entre 0 e 10.\033[m')
 print('\033[1;31mSerá que você consegue adivinhar qual foi ?\033[m')
 
+nome = str(input('Digite seu nome: '))
 acertou = False
 palpites = 0
 
@@ -17,4 +18,4 @@ while not acertou:
             print('Mais... Tente mais uma vez!')
         elif jogador > computador:
             print('Menos... Tente mais uma vez!')
-print('Acertou! com {} palpites. Parabéns'.format(palpites))
+print(f'{nome} Acertou! com {palpites} palpites. Parabéns')
